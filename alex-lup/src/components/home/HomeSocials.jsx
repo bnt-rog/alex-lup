5cabd73204c7487fa52269db3103789d74821266
BNT-ROG <bnt.roggeman@gmail.com>
2023-05-16 22:51:48 +0200

    ALEX-1: Home component

---
+++ alex-lup/src/components/home/HomeSocials.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const HomeSocials = () => {
+  return (
+    <div className="home__socials">
+        <a href="" className="home__social-link" target='_blank'>
+            <i class="fa-brands fa-instagram"></i>
+        </a>
+        <a href="" className="home__social-link" target='_blank'>
+            <i class="fa-brands fa-tiktok"></i>
+        </a>
+        <a href="" className="home__social-link" target='_blank'>
+            <i class="fa-brands fa-linkedin"></i>
+        </a>
+        <a href="" className="home__social-link" target='_blank'>
+            <i class="fa-brands fa-square-pinterest"></i>
+        </a>
+    </div>
+  )
+}
+
+export default HomeSocials
