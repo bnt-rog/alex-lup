2a3a98d231a6f0a0d78577b57285c221f6402a25
BNT-ROG <bnt.roggeman@gmail.com>
2023-05-22 21:54:28 +0200

    ALEX-1: test new project

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
-import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-import Sidebar from './components/sidebar/Sidebar';
-import Home from './components/home/Home';
-import About from './components/about/About';
-import Portfolio from './components/portfolio/Portfolio';
-import Contact from './components/contact/Contact';
 
 function App() {
   return (
-    <>
-    <Sidebar />
-    <main className='main'>
-      <Home />
-      <About />
-      <Portfolio />
-      <Contact />
-    </main>
-    </>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
